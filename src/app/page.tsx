e7f7edd83ae0e656dd73db744eea1346ae0bec58
Murilojms7 <murilojms7@gmail.com>
2024-04-08 21:48:30 -0300

    banner

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,24 @@
 import NavBar from "@/components/NavBar";
-import Image from "next/image";
+import Banner from "@/app/banner.png"
+import Carrinho from "@/components/Carrinho"
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center bg-gray-200">
       <NavBar active={"paginaInicial"}/>
+      
+      <div className="">
+        <img src="@/app/banner.png" alt="Banner Principal"/>
+      </div>
+      
+      <div className="">
+      <h1>Lançamentos</h1>
+      </div>
 
-      <h1 className="font-bold size-20">Página Inicial</h1>
+      <div className="flex gap-48">
+        <img src="https://nextui-docs-v2.vercel.app/images/fruit-1.jpeg" width={250}/>
+        
+      </div>
     </main>
   );
 }
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,39 +1,43 @@
 import Link from "next/link";
-
+import { Search, ShoppingCart } from "lucide-react";
+import Carrinho from "@/components/Carrinho"
+import Script from "next/script";
 interface NavBarProps{
     active: "basquete" | "futebol" | "login" | "paginaInicial"
 }
 export default function NavBar(props: NavBarProps ){
     const{ active } = props 
-    const classActive = "border-b-4  border-pink-500 pb-2"
+    const classActive = "border-b-4 border-pink-500 pb-2"
 
     return(
-        <nav className="bg-zinc-800 w-full justify-between px-20 pt-5 shadow-2xl font-bold">
-        <div className="flex gap-20 items-center  "> 
-            <div className="w-14 rounded-full overflow-hidden ">
+        <nav className="bg-zinc-800 w-full justify-between pt-5 shadow-2xl font-bold">
+        <div className="flex items-center px-80 gap-20"> 
+            <div className="w-14 rounded-full overflow-hidden">
                 <img src="https://i.pravatar.cc/150" alt="Avatar do usuário" />
+                
             </div>
 
             <h1 className="text-2xl text-center">Sports Jersey<br/> Kingdom</h1>
 
             <div className="bg-zinc-500 rounded-full basis-1/3 justify-between flex size-12 py-3 px-10"> 
                 <h1 className="opacity-40">Procurar Camisa</h1>
-                <h1 className="opacity-40">Lupa</h1>
+                <h1 className="opacity-40"><Search/></h1>
             </div>
 
-            <div className="">
+            <button id="botaoCarrinho" className="flex gap-3 bg-zinc-500 rounded-full py-2 px-2">
+                <Carrinho/>
+                <ShoppingCart/>
                 <h3>Carrinho</h3>
-            </div>
+            </button>
         </div>
         
-        <ul className="flex justify-between px-20">
+        <ul className="flex justify-between px-96">
         <li className={active == "paginaInicial" ? classActive : ""}><Link href="/">Pagina Inicial</Link></li>
           <li className={active == "basquete" ? classActive : ""}><Link href="/basquete">Basquete</Link></li>
           <li className={active == "futebol" ? classActive : ""}><Link href="/futebol">Futebol</Link></li>
           <li className={active == "login" ? classActive : ""} ><Link href="/login">Login</Link></li>
         </ul>
-        
       </nav>
-    )
 
+    ) 
 }
