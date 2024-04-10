4dabc5c0108c58477a919794488dea984e095305
Murilojms7 <murilojms7@gmail.com>
2024-04-10 10:40:03 -0300

    pagina

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,7 +1,6 @@
 import Link from "next/link";
 import { Search, ShoppingCart } from "lucide-react";
-import Script from "next/script";
-import Image from "next/image";
+
 
 interface NavBarProps{
     active: "basquete" | "futebol" | "login" | "paginaInicial"
@@ -11,27 +10,26 @@ export default function NavBar(props: NavBarProps ){
     const classActive = "border-b-4 border-pink-500 pb-2"
 
     return(
-        <nav className="bg-zinc-800 w-full">
+        <nav className="bg-zinc-700 w-full py-3 shadow-xl">
             <div className="mx-auto px-2 sm:px-6 lg:px-8">
-                <div className=" flex h-16 items-center justify-around  ">
-                    <div className=" ">
+                <div className=" flex h-16 items-center justify-around">
+                    <div>
                         <h1 className="text-2xl text-center text-white font-bold">Sports Jersey<br/> Kingdom</h1>
                     </div>
 
-                    <div className="hidden sm:block ">
+                    <div className="hidden sm:block">
                         <ul className="flex">
-                            <li className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium"><Link href="/">Pagina Inicial</Link></li>
-                            <li className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium"><Link href="/basquete">Basquete</Link></li>
-                            <li className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium"><Link href="/futebol">Futebol</Link></li>
-                            <li className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium" ><Link href="/login">Login</Link></li>
+                            <li className="text-gray-200 hover:bg-gray-500 hover:text-white rounded-md px-3 py-2 text-sm font-bold"><Link href="/">Pagina Inicial</Link></li>
+                            <li className="text-gray-200 hover:bg-gray-500 hover:text-white rounded-md px-3 py-2 text-sm font-bold"><Link href="/basquete">Basquete</Link></li>
+                            <li className="text-gray-200 hover:bg-gray-500 hover:text-white rounded-md px-3 py-2 text-sm font-bold"><Link href="/futebol">Futebol</Link></li>
+                            <li className="text-gray-200 hover:bg-gray-500 hover:text-white rounded-md px-3 py-2 text-sm font-bold" ><Link href="/login">Login</Link></li>
                         </ul>
                     </div>
                 
-                    <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-                        <button type="button" className="relative rounded-full bg-zinc-800 p-1 bg-zinc-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
+                    <div className="absolute inset-y-0 right-0  items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0 ">
+                        <button type="button" className="flex font-bold relative rounded-full bg-gray-800 p-1 bg-zinc-300 hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
+                        <ShoppingCart/>
                         <h1>Carrinho</h1>
-                        <span className="absolute -inset-1.5"></span>
-                        <span className="sr-only">View notifications</span>
                         </button>
                     </div>
 
