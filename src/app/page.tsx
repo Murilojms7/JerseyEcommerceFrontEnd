9ec6718d9127861c4eff110e3f5f7f547bc8d491
murilojms7 <murilojms7@gmail.com>
2024-04-10 09:27:07 -0300

    Pagina inicial

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,31 @@
 import NavBar from "@/components/NavBar";
 import Banner from "@/app/banner.png"
-import Carrinho from "@/components/Carrinho"
-
+ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center bg-gray-200">
       <NavBar active={"paginaInicial"}/>
       
       <div className="">
-        <img src="@/app/banner.png" alt="Banner Principal"/>
+        <Image
+              src="/banner.png"
+              alt="logo"
+              className="dark:invert pt-16  "
+              width={1000}
+              height={24}
+              priority
+        />
       </div>
       
-      <div className="">
-      <h1>Lançamentos</h1>
+      <div className="p-14">
+      <h1 className="text-2xl text-center text-white font-bold">Lançamentos</h1>
       </div>
 
-      <div className="flex gap-48">
-        <img src="https://nextui-docs-v2.vercel.app/images/fruit-1.jpeg" width={250}/>
-        
+      <div className="flex gap-20">
+        <img src="/camisa-alemanha.png" width={250}/>
+        <img src="/camisa-coreia.png" width={250}/>
+        <img src="/camisa-holanda.png" width={250}/>
+        <img src="/camisa-portugal.png" width={250}/>
       </div>
     </main>
   );
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,7 +1,8 @@
 import Link from "next/link";
 import { Search, ShoppingCart } from "lucide-react";
-import Carrinho from "@/components/Carrinho"
 import Script from "next/script";
+import Image from "next/image";
+
 interface NavBarProps{
     active: "basquete" | "futebol" | "login" | "paginaInicial"
 }
@@ -10,34 +11,39 @@ export default function NavBar(props: NavBarProps ){
     const classActive = "border-b-4 border-pink-500 pb-2"
 
     return(
-        <nav className="bg-zinc-800 w-full justify-between pt-5 shadow-2xl font-bold">
-        <div className="flex items-center px-80 gap-20"> 
-            <div className="w-14 rounded-full overflow-hidden">
-                <img src="https://i.pravatar.cc/150" alt="Avatar do usuário" />
+        <nav className="bg-zinc-800 w-full">
+            <div className="mx-auto  px-2 sm:px-6 lg:px-8">
+                <div className=" flex h-16 ">
+
+                    <div className="flex items-center ">
+                        <Image src="/banner.png" alt="logo" className="dark:invert" width={100} height={24} priority/>
+                    </div>
+
+                    <div>
+                        <h1 className="text-2xl text-center text-white font-bold">Sports Jersey<br/> Kingdom</h1>
+                    </div>
+
+                    <div className="hidden sm:block items-center  ">
+                        <ul className="flex">
+                            <li className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium"><Link href="/">Pagina Inicial</Link></li>
+                            <li className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium"><Link href="/basquete">Basquete</Link></li>
+                            <li className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium"><Link href="/futebol">Futebol</Link></li>
+                            <li className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium" ><Link href="/login">Login</Link></li>
+                        </ul>
+                    </div>
                 
+                    <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+                        <button type="button" className="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
+                        <span className="absolute -inset-1.5"></span>
+                        <span className="sr-only">View notifications</span>
+                        <svg className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0" />
+                        </svg>
+                        </button>
+                    </div>
+
+                </div>
             </div>
-
-            <h1 className="text-2xl text-center">Sports Jersey<br/> Kingdom</h1>
-
-            <div className="bg-zinc-500 rounded-full basis-1/3 justify-between flex size-12 py-3 px-10"> 
-                <h1 className="opacity-40">Procurar Camisa</h1>
-                <h1 className="opacity-40"><Search/></h1>
-            </div>
-
-            <button id="botaoCarrinho" className="flex gap-3 bg-zinc-500 rounded-full py-2 px-2">
-                <Carrinho/>
-                <ShoppingCart/>
-                <h3>Carrinho</h3>
-            </button>
-        </div>
-        
-        <ul className="flex justify-between px-96">
-        <li className={active == "paginaInicial" ? classActive : ""}><Link href="/">Pagina Inicial</Link></li>
-          <li className={active == "basquete" ? classActive : ""}><Link href="/basquete">Basquete</Link></li>
-          <li className={active == "futebol" ? classActive : ""}><Link href="/futebol">Futebol</Link></li>
-          <li className={active == "login" ? classActive : ""} ><Link href="/login">Login</Link></li>
-        </ul>
-      </nav>
-
+        </nav>
     ) 
 }
